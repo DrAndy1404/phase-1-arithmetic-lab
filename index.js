0b90db96233deb23577ce2d7781976a26b7be5b7
Andrew <andrew.dimou10@gmail.com>
2024-01-03 10:48:17 +1100

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// 1) is an equation whose multiplied results will equal 62
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
 
-// Write your code here
+//  2) generates a random integer greater than 0
+var random = Math.floor(Math.random() * 100) + 1;
+
+// 3) is an equation that calculates a remainder that is equal to 4
+var num3 = 17;
+var num4 = 13; // Assuming num4 value, change accordingly
+var mod = num3 % num4;
+
+//  4) will return 20 as the highest number in the set
+var numbers = [10, 5, 20, 15];
+var max = Math.max(...numbers);
+
